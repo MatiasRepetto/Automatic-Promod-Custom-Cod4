8b8406682b4e428c4b872ff579f47cc0383e9d61
MatiasRepetto <repetto.matias3@gmail.com>
2022-10-08 22:28:16 -0300

    txt creation working

--- src/utils.cpp
+++ src/utils.cpp
@@ -175,7 +175,7 @@ void utils::writefile(){
         "}"
     };
     try {
-        std::ofstream fw("./resources/promod4-master/promod_ruleset/custom_public.txt", std::ofstream::out);
+        std::ofstream fw("C:\\Users\\dd\\Desktop\\Automatic-Promod-Custom-Cod4\\resources\\promod4-master\\promod_ruleset\\custom_public.txt", std::ofstream::out);
         if (fw.is_open())
         {
         for (int i = 0; i < arrayTam; i++) {
