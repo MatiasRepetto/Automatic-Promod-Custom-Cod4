8e177d7123e61a782380e99d05b9622e5db96066
MatiasRepetto <repetto.matias3@gmail.com>
2022-10-08 21:14:44 -0300

    utils file txt

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,12 @@
 #ifndef UTILS
 #define UTILS
 
+#include <iostream>
+#include <stdio.h>
+
 class utils{
     public:
+        static void writefile();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 #include <stdio.h>
 
 int main(){
-    
-    return 0;
+    int mMint;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "(0) Exit" << std::endl;
+    std::cin >> mMint;
+    switch(mMint){
+        case 1:
+            return main();
+        case 2:
+            return main();
+        case 3:
+            return main();
+        case 0:
+            return 0;
+        default:
+            return main();
+    }
 }
