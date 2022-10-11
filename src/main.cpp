59980251756c1bd77ca8b250abed271ccbac4d77
MatiasRepetto <repetto.matias3@gmail.com>
2022-10-11 20:32:00 -0300

    frag vars declared in the utils.h, frag mods already concatenated in txt, menu WIP

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int main(){
+    system("cls");
     varTxt_v aux;
     int mMint;
     std::cout << "(1) Edit Class Limits" << std::endl;
@@ -14,6 +15,7 @@ int main(){
     std::cin >> mMint;
     switch(mMint){
         case 1:
+            system("cls");
             int case1Aux;
             std::cout << "(1) Edit Limit of Assoult class" << std::endl;
             std::cout << "(2) Edit Limit of Specops class" << std::endl;
@@ -23,21 +25,25 @@ int main(){
             std::cin >> case1Aux;
             switch(case1Aux){
                 case 1:
+                    system("cls");
                     std::cout << "Enter Limit of Assoult classes (1-64)" << std::endl;
                     std::cin >> aux.cAssoult;
                     aux.cAssoult = aux.cAssoult % 65;
                     return main();
                 case 2:
+                    system("cls");
                     std::cout << "Enter Limit of Specops classes (1-64)" << std::endl;
                     std::cin >> aux.cSpecops;
                     aux.cSpecops = aux.cSpecops % 65;
                     return main();
                 case 3:
+                    system("cls");
                     std::cout << "Enter Limit of Demolition classes (1-64)" << std::endl;
                     std::cin >> aux.cDemolitions;
                     aux.cDemolitions = aux.cDemolitions % 65;
                     return main();
                 case 4:
+                    system("cls");
                     std::cout << "Enter Limit of Sniper classes (1-64)" << std::endl;
                     std::cin >> aux.cSniper;
                     aux.cSniper = aux.cSniper % 65;
@@ -48,6 +54,7 @@ int main(){
                     return main();
             }
         case 2:
+            system("cls");
             int case2Aux;
             std::cout << "(1) Edit Score Limit" << std::endl;
             std::cout << "(2) Edit Time Limit" << std::endl;
@@ -55,10 +62,12 @@ int main(){
             std::cin >> case2Aux;
             switch(case2Aux){
                 case 1:
+                    system("cls");
                     std::cout << "Enter Score Limit" << std::endl;
                     std::cin >> aux.tdmScoreLimit;
                     return main();
                 case 2:
+                    system("cls");
                     std::cout << "Enter Time Limit (Minutes)" << std::endl;
                     std::cin >> aux.tdmTimeLimit;
                     return main();
@@ -68,6 +77,7 @@ int main(){
                     return main();
             }
         case 3:
+            system("cls");
             int case3Aux;
             std::cout << "(1) Edit Score Limit" << std::endl;
             std::cout << "(2) Edit Time Limit" << std::endl;
@@ -76,14 +86,17 @@ int main(){
             std::cin >> case3Aux;
             switch(case3Aux){
                 case 1:
+                    system("cls");
                     std::cout << "Enter Score Limit" << std::endl;
                     std::cin >> aux.dmScoreLimit;
                     return main();
                 case 2:
+                    system("cls");
                     std::cout << "Enter Time Limit (Minutes)" << std::endl;
                     std::cin >> aux.dmTimeLimit;
                     return main();
                 case 3:
+                    system("cls");
                     std::cout << "Enter Spawn Delay (Seconds)" << std::endl;
                     std::cin >> aux.dmSpawnDelay;
                     return main();
@@ -93,14 +106,16 @@ int main(){
                     return main();
             }
         case 4:
+            system("cls");
             int case4Aux;
             std::cout << "(1) Edit Flash Grenade" << std::endl;
             std::cout << "(2) Edit Frag Grenade" << std::endl;
-            std::cout << "(3) Edit Smoake Grenade" << std::endl;
+            std::cout << "(3) Edit Smoke Grenade" << std::endl;
             std::cout << "(0) Back" << std::endl;
             std::cin >> case4Aux;
             switch(case4Aux){
                 case 1:
+                    system("cls");
                     int flashAux;
                     std::cout << "(1) Allowed" << std::endl;
                     std::cout << "(0) Not allowed" << std::endl;
@@ -115,6 +130,7 @@ int main(){
                     }
                     return main();
                 case 2:
+                    system("cls");
                     int fragAux;
                     std::cout << "(1) Allowed" << std::endl;
                     std::cout << "(0) Not allowed" << std::endl;
@@ -129,6 +145,7 @@ int main(){
                     }
                     return main();
                 case 3:
+                    system("cls");
                     int smokeAux;
                     std::cout << "(1) Allowed" << std::endl;
                     std::cout << "(0) Not allowed" << std::endl;
@@ -153,6 +170,7 @@ int main(){
             utils::writefile(aux);
             system("cd .\\resources\\promod4-master\\promod_ruleset && del custom_public.gsc && ren *.txt *.gsc");
             system(".\\resources\\promod4-master\\compile.bat");
+            system("cls");
             break;
         default:
             return main();
